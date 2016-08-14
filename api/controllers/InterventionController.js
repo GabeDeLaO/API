8756f3fd185845dfeecd2a6217c9dc65693bf9f9
Gabriel De La O <gabriel@cohesionlabs.io>
2016-08-13 21:39:39 -0600

    Added Interventions, Notifications, and Settings to API.

---
+++ api/controllers/InterventionController.js
@@ -0,0 +1,11 @@
+/**
+ * InterventionController
+ *
+ * @description :: Server-side logic for managing interventions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
