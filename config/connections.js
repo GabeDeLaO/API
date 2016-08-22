4d5737e66a5a6eed26ac50b7409a1ea07b528e8c
Gabriel De La O <gabriel@cohesionlabs.io>
2016-08-21 19:35:40 -0500

    saving settings.

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  someMongodbServer: {
+     adapter: 'sails-mongo',
+     host: 'ds013366.mlab.com',
+     port: 13366,
+     user: 'test', //optional
+     password: 'test', //optional
+     database: 'apapi' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
